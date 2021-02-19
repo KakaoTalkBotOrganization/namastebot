4fd37b292a68aa681ed1d9b893632f90160bcd02
PartWorm <partworm@gmail.com>
2021-02-20 05:18:52 +0900

    Add test bot

---
+++ app/src/main/java/com/partworm/namastebot/Bot.kt
@@ -0,0 +1,49 @@
+package com.partworm.namastebot
+
+import android.app.Notification
+import android.content.Context
+import com.faendir.rhino_android.RhinoAndroidHelper
+import org.mozilla.javascript.Context as RhinoContext
+import org.mozilla.javascript.Function
+import org.mozilla.javascript.Scriptable
+
+class Replier(
+	private val context: Context,
+	private val session: Notification.Action,
+) {
+
+	fun reply(message: String) {
+		send_to_session(context, session, message)
+	}
+
+}
+
+class Bot(private val context: Context) {
+
+	private var scope: Scriptable
+
+	private val responder: Function?
+
+	init {
+		val rhino_context =
+			RhinoAndroidHelper(context).enterContext()
+				.apply { languageVersion = RhinoContext.VERSION_ES6 }
+		scope = rhino_context.initStandardObjects()
+		rhino_context.compileString("""
+			function response(replier) {
+				replier.reply("Hello world");
+			}
+		""".trimIndent(), "", 1, null).exec(rhino_context, scope)
+		responder = scope["response", scope] as? Function
+		RhinoContext.exit()
+	}
+
+	fun execute(session: Notification.Action) {
+		val rhino_context =
+			RhinoAndroidHelper(context).enterContext()
+				.apply { languageVersion = RhinoContext.VERSION_ES6 }
+		responder?.call(rhino_context, scope, scope, arrayOf(Replier(context, session)))
+		RhinoContext.exit()
+	}
+
+}
--- app/src/main/java/com/partworm/namastebot/NBNotificationListenerService.kt
+++ app/src/main/java/com/partworm/namastebot/NBNotificationListenerService.kt
@@ -2,11 +2,14 @@ package com.partworm.namastebot
 
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
+import android.util.Log
 import androidx.core.content.pm.PackageInfoCompat
 
 class NBNotificationListenerService : NotificationListenerService() {
 
 	var kakaotalk_version: Long = 0
+
+	var bot: Bot? = null
 
 	override fun onCreate() {
 		super.onCreate()
@@ -17,11 +20,24 @@ class NBNotificationListenerService : NotificationListenerService() {
 		}
 		catch (_: Exception) {
 		}
+		bot = Bot(this)
 	}
 
 	override fun onNotificationPosted(noti: StatusBarNotification) {
 		super.onNotificationPosted(noti)
 		val message = parse_message_from_notification(noti, kakaotalk_version) ?: return
+		Log.d("namastebot", """
+			room: ${message.room}
+			sender: ${message.sender}
+			content: ${message.content}
+			isGroupChat: ${message.isGroupChat}
+			packageName: ${message.packageName}
+		""".trimIndent())
+		val debug_sender = "FakeWorm"
+		if (message.sender != debug_sender) {
+			return
+		}
+		bot?.execute(message.action)
 	}
 
 }
